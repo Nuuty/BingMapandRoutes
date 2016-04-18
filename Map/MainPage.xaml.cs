0161c416745f5698a906c799d34b4032b361b1ff
Niels Odgaard <niel2647@edu.easj.dk>
2016-04-18 23:14:21 +0200

    Add project files.

---
+++ Map/MainPage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Services.Maps;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Map
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MapService.ServiceToken = "Ktkp6HLxn7P0k9ZXjNk5~xg8PB2ELRceZuBVhSx3TNw~AvEqNibDD9mn2tD1QZEcHfcGN6bNWBNaxOxOkhMIg_Iqla1KNrtyNWe12329eyaX";
+            //AddMapIcon();
+            GetRouteAndDirections();
+        }
+
+        /*protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Map.Center =
+                new Geopoint(new BasicGeoposition()
+                {
+                    Latitude = 47.604,
+                    Longitude = -122.329
+                });
+            Map.ZoomLevel = 12;
+            Map.LandmarksVisible = true;
+        }
+
+        private void AddMapIcon()
+        {
+            MapIcon MapIcon1 = new MapIcon();
+            MapIcon1.Location = new Geopoint(new BasicGeoposition()
+            {
+                Latitude = 47.620,
+                Longitude = -122.349
+            });
+            MapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+            MapIcon1.Title = "Space Needle";
+            Map.MapElements.Add(MapIcon1);
+        }*/
+
+        private async void GetRouteAndDirections()
+        {
+            // Start at Microsoft in Redmond, Washington.
+            BasicGeoposition startLocation = new BasicGeoposition();
+            startLocation.Latitude = 47.643;
+            startLocation.Longitude = -122.131;
+            Geopoint startPoint = new Geopoint(startLocation);
+
+            // End at the city of Seattle, Washington.
+            BasicGeoposition endLocation = new BasicGeoposition();
+            endLocation.Latitude = 47.604;
+            endLocation.Longitude = -122.329;
+            Geopoint endPoint = new Geopoint(endLocation);
+
+            // Get the route between the points.
+            MapRouteFinderResult routeResult =
+                await MapRouteFinder.GetDrivingRouteAsync(
+                startPoint,
+                endPoint,
+                MapRouteOptimization.Time,
+                MapRouteRestrictions.None);
+
+            if (routeResult.Status == MapRouteFinderStatus.Success)
+            {
+                // Display summary info about the route.
+                tbOutputText.Inlines.Add(new Run()
+                {
+                    Text = "Total estimated time (minutes) = "
+                        + routeResult.Route.EstimatedDuration.TotalMinutes.ToString()
+                });
+                tbOutputText.Inlines.Add(new LineBreak());
+                tbOutputText.Inlines.Add(new Run()
+                {
+                    Text = "Total length (kilometers) = "
+                        + (routeResult.Route.LengthInMeters / 1000).ToString()
+                });
+                tbOutputText.Inlines.Add(new LineBreak());
+                tbOutputText.Inlines.Add(new LineBreak());
+
+                // Display the directions.
+                tbOutputText.Inlines.Add(new Run()
+                {
+                    Text = "DIRECTIONS"
+                });
+                tbOutputText.Inlines.Add(new LineBreak());
+
+                foreach (MapRouteLeg leg in routeResult.Route.Legs)
+                {
+                    foreach (MapRouteManeuver maneuver in leg.Maneuvers)
+                    {
+                        tbOutputText.Inlines.Add(new Run()
+                        {
+                            Text = maneuver.InstructionText
+                        });
+                        tbOutputText.Inlines.Add(new LineBreak());
+                    }
+                }
+            }
+            else
+            {
+                tbOutputText.Text =
+                    "A problem occurred: " + routeResult.Status.ToString();
+            }
+
+        }
+    }
+}
